47ef3d5c611b576f32d531da2cd52deccdf82572
v-pvithalkar <v-pvithalkar@microsoft.com>
2023-10-05 12:56:22 +0530

    updated the final project code

--- Countries_Data_Visualization_3/scripts/main.js
+++ Countries_Data_Visualization_3/scripts/main.js
@@ -1,70 +1,79 @@
 import countries_data from "./countries_data.js"
 
-const capitalSortedCountries = [...countries_data]
-capitalSortedCountries.sort((a, b) => {
-    if (a.capital < b.capital) {
-        return -1;
-    } else if (a.capital > b.capital) {
-        return 1;
-    }
-})
-const populationSortedCountries = [...countries_data]
-populationSortedCountries.sort((a, b) => {
-    if (a.population < b.population) {
-        return -1;
-    } else if (a.population > b.population) {
-        return 1;
-    }
-})
+// sort the array based on their capital city in alphabetical order
+function sortByCapital(countriesData) {
+    const capitalSortedCountries = [...countriesData]
+    capitalSortedCountries.sort((a, b) => {
+        if (a.capital < b.capital) {
+            return -1;
+        } else if (a.capital > b.capital) {
+            return 1;
+        }
+    })
+    return capitalSortedCountries
+}
+
+// sort the array based on their population
+function sortByPopulation(countriesData) {
+    const populationSortedCountries = [...countriesData]
+    populationSortedCountries.sort((a, b) => {
+        if (a.population < b.population) {
+            return -1;
+        } else if (a.population > b.population) {
+            return 1;
+        }
+    })
+    return populationSortedCountries
+}
 
 // Function to get top 10 countries by population
-function getTop10Countries(){
-    countries_data.sort((a,b) => b.population - a.population);
-    const top10Countries = countries_data.slice(0,10);
+function getTop10Countries(countries) {
+    countries.sort((a, b) => b.population - a.population);
+    const top10Countries = countries.slice(0, 10);
     return top10Countries
 }
 
 // get top 10 most spoken languages in the world
-function getTop10Languages(){
+function getTop10Languages(countries) {
     const languageCount = {};
 
-    countries_data.forEach(country => {
+    countries.forEach(country => {
         const languages = country.languages;
         languages.forEach(language => {
-            if(languageCount[language]){
+            if (languageCount[language]) {
                 languageCount[language]++;
             }
-            else{
+            else {
                 languageCount[language] = 1;
             }
-        }) 
+        })
     })
 
-    const languageArray = Object.entries(languageCount).map(([language,count]) => ({language, count}))
+    const languageArray = Object.entries(languageCount).map(([language, count]) => ({ language, count }))
 
-    languageArray.sort((a,b) => b.count - a.count)
+    languageArray.sort((a, b) => b.count - a.count)
 
-    const top10Languages = languageArray.slice(0,10)
+    const top10Languages = languageArray.slice(0, 10)
 
     return top10Languages
 }
 
 // create the chart showing top 10 most populated countries
-function getCountryChart(charts){
+function getCountryChart(charts,countries) {
     charts.innerHTML = ''
     const canvas = document.createElement('canvas')
     canvas.width = '900px'
     canvas.heihgt = '600px'
     charts.appendChild(canvas)
     const ctx = canvas.getContext('2d')
-    const top10Countries = getTop10Countries()
-    let countryNames  = []
+    const top10Countries = getTop10Countries(countries)
+    let countryNames = []
     let population = []
-    for (let country of top10Countries){
+    for (let country of top10Countries) {
         countryNames.push(country.name)
         population.push(country.population)
     }
-    const myChart = new Chart(ctx,{
+    const myChart = new Chart(ctx, {
         type: "bar",
         data: {
             labels: countryNames,
@@ -85,7 +94,7 @@ function getCountryChart(charts){
                 },
                 y: {
                     beginAtZero: true,
-                    grid:{
+                    grid: {
                         display: false
                     }
                 }
@@ -105,32 +114,33 @@ function getCountryChart(charts){
 
     myChart.config.data.datasets.forEach(dataset => {
         for (let i = 0; i < dataset.data.length; i++) {
-            const model = myChart.getDatasetMeta(0).data[i]._model;
+            const model = myChart.getDatasetMeta(0).data[i]
+            console.log(model)
             const value = dataset.data[i];
             ctx.fillStyle = 'black'; // Label text color
             ctx.font = '14px Arial'; // Label font style
             ctx.fillText(value, model.x + 10, model.y + model.height / 2 + 6); // Adjust the coordinates for label placement
         }
     });
-    
+
 }
 
 // Create the chart showing top 10 most spoken languages in the world
-function getLanguageChart(charts){
+function getLanguageChart(charts,countries) {
     charts.innerHTML = ''
     const canvas = document.createElement('canvas')
     canvas.width = '900px'
     canvas.heihgt = '600px'
     charts.appendChild(canvas)
     const ctx = canvas.getContext('2d')
-    const top10Languages = getTop10Languages()
+    const top10Languages = getTop10Languages(countries)
     let languages = []
     let counts = []
-    for (let i=0;i<top10Languages.length;i++){
+    for (let i = 0; i < top10Languages.length; i++) {
         languages.push(top10Languages[i].language)
         counts.push(top10Languages[i].count)
     }
-    const myChart = new Chart(ctx,{
+    const myChart = new Chart(ctx, {
         type: "bar",
         data: {
             labels: languages,
@@ -151,7 +161,7 @@ function getLanguageChart(charts){
                 },
                 y: {
                     beginAtZero: true,
-                    grid:{
+                    grid: {
                         display: false
                     }
                 }
@@ -171,7 +181,7 @@ function getLanguageChart(charts){
 
     const datasetMeta = myChart.getDatasetMeta(0);
 
-    if (datasetMeta && datasetMeta.data){
+    if (datasetMeta && datasetMeta.data) {
         datasetMeta.data.forEach((bar, i) => {
             const model = bar._model;
             if (model) {
@@ -194,6 +204,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const charts = document.getElementById("charts")
     const textInput = document.getElementById("searchText")
     let filteredCountries = [...countries_data]
+    let capitalSortedCountries = sortByCapital(filteredCountries)
+    let populationSortedCountries = sortByPopulation(filteredCountries)
+
+    // getCountryChart(charts)
+    // getTop10Countries(filteredCountries)
 
 
     for (let c of countries_data) {
@@ -206,7 +221,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <p>Population:${c.population}</p>`
         countryList.appendChild(country)
     }
-    // getCountryChart(charts)
+    getCountryChart(charts,filteredCountries)
 
     nameSort.addEventListener("click", function () {
 
@@ -257,19 +272,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
-    popButton.addEventListener("click",function(){
-        getCountryChart(charts)
+    popButton.addEventListener("click", function () {
+        getCountryChart(charts,filteredCountries)
     })
 
-    langButton.addEventListener("click",function(){
-        getLanguageChart(charts)
+    langButton.addEventListener("click", function () {
+        getLanguageChart(charts,filteredCountries)
     })
 
-    textInput.addEventListener("input",() => { 
+    textInput.addEventListener("input", () => {
         const searchText = textInput.value
         // console.log(searchText)
         filteredCountries = countries_data.filter(item => item.name.toLowerCase().includes(searchText) ||
-        item.capital.toLowerCase().includes(searchText) || item.languages.join(",").toLowerCase().includes(searchText))
+            item.capital.toLowerCase().includes(searchText) || item.languages.join(",").toLowerCase().includes(searchText))
+        capitalSortedCountries = sortByCapital(filteredCountries)
+        populationSortedCountries = sortByPopulation(filteredCountries)
         countryList.innerHTML = ""
 
         for (let c of filteredCountries) {
